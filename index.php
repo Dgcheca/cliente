d272b360349aaa04c4b86006ddf7bf438dd59e2a
Daniel Gomez Checa <mardukecheka@gmail.com>
2022-02-22 13:15:38 +0100

    2202

---
+++ index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Proyectos Cliente</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+</head>
+
+
+<body>
+
+  <div class="row mb-3">
+    <div class="col-md-2 themed-grid-col">
+
+      <div class="flex-shrink-0 p-3 bg-white" style="width: 280px;">
+          <span class="fs-5 fw-semibold text-primary d-flex align-items-center pb-3 mb-3 link-dark border-bottom">
+            EJERCICIOS JS --  <a href="/" class="text-decoration-none text-black"> Atras</a>
+
+          </span>
+      
+        <ul class="list-unstyled ps-0">
+          <li class="mb-1">
+            <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#tema7" aria-expanded="false">
+              TEMA 7
+            </button>
+            <div class="collapse" id="tema7">
+              <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                <li><a href="Tema7/relacion1/ejercicio1.html" class="link-dark rounded">Ejercicio1</a></li>
+                <li><a href="Tema7/relacion1/ejercicio2.html" class="link-dark rounded">Ejercicio2</a></li>
+                <li><a href="Tema7/relacion1/ejercicio3.html" class="link-dark rounded">Ejercicio3</a></li>
+                <li><a href="Tema7/relacion1/ejercicio4.html" class="link-dark rounded">Ejercicio4</a></li>
+                <li><a href="Tema7/relacion1/ejercicio7.html" class="link-dark rounded">Ejercicio7</a></li>
+                <li><a href="Tema7/relacion1/ejercicio8.html" class="link-dark rounded">Ejercicio8</a></li>
+              </ul>
+            </div>
+          </li>
+          
+        </ul>
+        <ul class="list-unstyled ps-0">
+          <li class="mb-1">
+            <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#tema8" aria-expanded="false">
+              Angular
+            </button>
+            <div class="collapse" id="tema8">
+              <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                -- EN CONSTRUCCION --
+              </ul>
+            </div>
+          </li>
+          
+        </ul>
+      </div>
+
+    </div>
+  </div>
+  <script src="bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
